7d00899529b8f76968a13f7c7405a119e4f95a86
Pouya Arvandian <pouya314@gmail.com>
2014-04-16 12:14:41 +0800

    with a before_save callback (at the active record level) ensure team is not submitting answer to a challenge (that they have solved before) again. They could be tempted to do this to gain more points.

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -1,4 +1,13 @@
 class Solution < ActiveRecord::Base
   belongs_to :challenge
   belongs_to :team
+  
+  before_save :ensure_team_does_not_submit_same_answer_twice
+  
+  private
+    def ensure_team_does_not_submit_same_answer_twice
+      if Solution.where({challenge_id: self.challenge_id, team_id: self.team_id}).exists?
+        false
+      end
+    end
 end
