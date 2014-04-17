7442f618dfcfcdea305860e0bed076f5bd237900
Pouya Arvandian <pouya314@gmail.com>
2014-04-17 17:24:31 +0800

    work in progress on charts..

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,6 +3,8 @@ class IndexController < ApplicationController
   end
 
   def chart
+    @time = Team.last.created_at.utc.to_i*1000
+    logger.debug "time passed to template => #{@time}"
   end
 
   def screen
