1cdbe44587a95d93968ffd0d8d7d88ac84da7b84
Pouya Arvandian <pouya314@gmail.com>
2014-04-19 20:24:10 +0800

    status page done. needs more improvements though...

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,16 +1,38 @@
 class IndexController < ApplicationController
   layout 'publictem'
-  
+
   def status
-    # Challenge.order(created_at: :asc).select(:id).map {|i| i.id }
+    challs = Challenge.order(created_at: :asc)
+
+    @ch_names = challs.map {|i| i.name}
+    ordered_challenges = challs.map {|i| i.id}
+
+    @tabledata = {}
+    Team.all.each do |team|
+      solved = team.solutions.select(:challenge_id).map {|i| i.challenge_id}
+      team_scores_list = []
+
+      for challenge in ordered_challenges
+        if solved.include? challenge
+          team_scores_list << 1
+        else
+          team_scores_list << 0
+        end
+      end
+
+      @tabledata[team.name] = team_scores_list
+    end
   end
+
 
   def chart
     @teams = Team.all
   end
 
+
   def screen
   end
+
 
   def ranking
     @ranking_list = Team.order(current_score: :desc)
