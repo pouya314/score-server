77842d7126d2df46abd529707b041530e247132d
Pouya Arvandian <pouya314@gmail.com>
2014-04-22 10:17:02 +0800

    cleaned up challenges controller a little bit. removed the unnecessary actions that are not used.

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -19,59 +19,12 @@ class ChallengesController < ApplicationController
     end
   end
 
+
   # GET /challenges/1
   # GET /challenges/1.json
   def show
   end
 
-  # GET /challenges/new
-  def new
-    @challenge = Challenge.new
-  end
-
-  # GET /challenges/1/edit
-  def edit
-  end
-
-  # POST /challenges
-  # POST /challenges.json
-  def create
-    @challenge = Challenge.new(challenge_params)
-
-    respond_to do |format|
-      if @challenge.save
-        format.html { redirect_to challenges_path, notice: 'Challenge was successfully created.' }
-        format.json { render :show, status: :created, location: @challenge }
-      else
-        format.html { render :new }
-        format.json { render json: @challenge.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /challenges/1
-  # PATCH/PUT /challenges/1.json
-  def update
-    respond_to do |format|
-      if @challenge.update(challenge_params)
-        format.html { redirect_to @challenge, notice: 'Challenge was successfully updated.' }
-        format.json { render :show, status: :ok, location: @challenge }
-      else
-        format.html { render :edit }
-        format.json { render json: @challenge.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /challenges/1
-  # DELETE /challenges/1.json
-  def destroy
-    @challenge.destroy
-    respond_to do |format|
-      format.html { redirect_to challenges_url }
-      format.json { head :no_content }
-    end
-  end
 
   # Ajax call that verifies answer submitted
   # and returns appropriate response (in JS format)
@@ -95,15 +48,18 @@ class ChallengesController < ApplicationController
     )
   end
 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_challenge
       @challenge = Challenge.find(params[:id])
     end
 
+
     def set_team
       @team = Team.find(params[:team_id])
     end
+
 
     def ensure_team_does_not_submit_same_answer_twice
       if Solution.where({challenge_id: @challenge.id, team_id: @team.id}).exists?
@@ -111,11 +67,13 @@ class ChallengesController < ApplicationController
       end
     end
 
+
     def ensure_team_cannot_access_show_page_of_a_challenge_that_they_have_already_solved
       if Solution.where({challenge_id: @challenge.id, team_id: current_team.id}).exists?
         redirect_to challenges_url, notice: "You cannot access that challenge, because you have already solved it!"
       end
     end
+
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def challenge_params
